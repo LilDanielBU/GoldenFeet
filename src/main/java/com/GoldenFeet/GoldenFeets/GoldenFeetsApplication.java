73bdd651e7a65bfe09f39b408f50f4847129819f
haviss <havidaltamar@gmail.com>
2025-12-08 14:28:38 -0500

    railway

--- src/main/java/com/GoldenFeet/GoldenFeets/GoldenFeetsApplication.java
+++ src/main/java/com/GoldenFeet/GoldenFeets/GoldenFeetsApplication.java
@@ -1,22 +1,14 @@
+
 package com.GoldenFeet.GoldenFeets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync; // <--- Importante
 
 @SpringBootApplication
-public class
-
-GoldenFeetsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GoldenFeetsApplication.class, args);
-	}
-
+@EnableAsync // <--- Agrega esto aquí
+public class GoldenFeetsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoldenFeetsApplication.class, args);
+    }
 }
-
-
-
-
-
-
-
